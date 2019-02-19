550988d737f75a67a6a11787521b4482f71c0893
enetto1 <ederr_2004@hotmail.com>
2019-02-19 14:51:41 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Product prod;
+		
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();	
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		int quantity = sc.nextInt();
+		
+		
+		
+		sc.close();
+	}
+
+}
